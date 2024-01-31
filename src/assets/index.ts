d58c19d08fafc2ba47584e43f5923cd01d46f1c0
Craig Smith <craig@littlejourney.health>
2024-01-31 18:40:50 +0000

    Added player 'move' animation.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,13 @@
 import { BitmapAsset, BitmapFontFace, BitmapSpriteAsset, Tileset } from "@overreact/engine";
 
 import arcadeFontUrl from "./arcade.png";
-import enemy1RunUrl from "./enemy-1-run.png";
 import enemy1IdleUrl from "./enemy-1-idle.png";
+import enemy1RunUrl from "./enemy-1-run.png";
 import enemy1StunnedUrl from "./enemy-1-stunned.png";
 import itemsUrl from "./items.png";
 import mysteryUrl from "./mystery.png";
-import player1Url from "./player1.png";
+import player1IdleUrl from "./player-1-idle.png";
+import player1RunUrl from "./player-1-run.png";
 import pointsUrl from "./points.png";
 import tilesetUrl from "./tileset.png";
 import zapUrl from "./zap.png";
@@ -29,9 +30,18 @@ export const TILESET: Tileset = {
   gridSize: [32, 25],
 };
 
-export const PLAYER1_IMAGE: BitmapAsset = {
-  url: player1Url,
+export const PLAYER_1_IDLE: BitmapSpriteAsset = {
+  url: player1IdleUrl,
   size: [16, 16],
+  count: 1,
+  rate: 10,
+};
+
+export const PLAYER_1_RUN: BitmapSpriteAsset = {
+  url: player1RunUrl,
+  size: [144, 16],
+  count: 9,
+  rate: 30,
 };
 
 export const ITEM_IMAGE: BitmapAsset = {
