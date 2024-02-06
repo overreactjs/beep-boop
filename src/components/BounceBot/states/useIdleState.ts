d175c47f572612bb77fc7492723a39619aeada14
Craig Smith <craig@littlejourney.health>
2024-02-06 22:14:04 +0000

    Randomized the pauses between bounces, so that bounce bots don't all jump in unison.

--- src/components/BounceBot/states/useIdleState.ts
+++ src/components/BounceBot/states/useIdleState.ts
@@ -1,12 +1,16 @@
 import { useCallback } from "react";
 import { StateFunction } from "../types";
+import { useProperty } from "@overreact/engine";
 
 export const useIdleState = (): StateFunction => {
+  const cooldown = useProperty(300);
+
   return useCallback((fsm) => {
     fsm.entity.animation.current = 'idle';
 
-    if (fsm.age > 300) {
+    if (fsm.age > cooldown.current) {
       fsm.replace('jumping');
+      cooldown.current = 200 + Math.random() * 150;
     }
-  }, []);
+  }, [cooldown]);
 };
