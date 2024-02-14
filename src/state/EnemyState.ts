5e56efd81fee4eb0c00a4d3213f3aec064a14dc5
Craig Smith <craig@littlejourney.health>
2024-02-14 11:23:16 +0000

    Simplify the enemy state classes.

--- src/state/EnemyState.ts
+++ src/state/EnemyState.ts
@@ -2,27 +2,23 @@ import { DynamicProperty, Position, Property, VariableProperty, Velocity } from 
 import { PositionedObjectState } from "./PositionedObjectState";
 import { Direction, EnemyType } from "../types";
 
+/**
+ * Basic stuff that all enemies need so that they can be stunned and bounced off the screen.
+ */
 class BaseEnemyState extends PositionedObjectState {
   velocity: Property<Velocity>;
   angle: Property<number>;
   scale: Property<number>;
   animation: Property<string>;
+  direction: Property<Direction>;
+  flip: Property<boolean>;
 
-  constructor(pos: Position) {
+  constructor(pos: Position, direction: Direction) {
     super(pos);
     this.velocity = new VariableProperty([0, 0]);
     this.angle = new VariableProperty(0);
     this.scale = new VariableProperty(1);
     this.animation = new VariableProperty('idle');
-  }
-}
-
-class BasePlatformEnemyState extends BaseEnemyState {
-  direction: Property<Direction>;
-  flip: Property<boolean>;
-
-  constructor(pos: Position, direction: Direction) {
-    super(pos);
     this.direction = new VariableProperty(direction);
     this.flip = new DynamicProperty(this.direction, (direction) => direction === 'left');
   }
@@ -32,34 +28,19 @@ class BasePlatformEnemyState extends BaseEnemyState {
   }
 }
 
-class BaseFlyingEnemyState extends BaseEnemyState {
-  direction: Property<Direction>;
-  flip: Property<boolean>;
-
-  constructor(pos: Position, direction: Direction) {
-    super(pos);
-    this.direction = new VariableProperty(direction);
-    this.flip = new DynamicProperty(this.direction, (direction) => direction === 'left');
-  }
-
-  reverse() {
-    this.direction.current = this.direction.current === 'left' ? 'right' : 'left';
-  }
-}
-
-export class BounceBotState extends BasePlatformEnemyState {
+export class BounceBotState extends BaseEnemyState {
   readonly type: EnemyType = 'bounceBot';
 }
 
-export class FlyingBotState extends BaseFlyingEnemyState {
+export class FlyingBotState extends BaseEnemyState {
   readonly type: EnemyType = 'flyingBot';
 }
 
-export class GuardBotState extends BasePlatformEnemyState {
+export class GuardBotState extends BaseEnemyState {
   readonly type: EnemyType = 'guardBot';
 }
 
-export class SecurityBotState extends BasePlatformEnemyState {
+export class SecurityBotState extends BaseEnemyState {
   readonly type: EnemyType = 'securityBot';
 }
 
