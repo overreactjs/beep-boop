10868db91be4e58c541211938e57f350f64b2e68
Craig Smith <craig@littlejourney.health>
2024-05-13 19:21:06 +0100

    Fixed the colors of the top bar text elements.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -9,8 +9,8 @@ export const TopBar: React.FC = () => {
   return (
     <Node>
       <ArcadeText pos={[16, 0]} color="green" text="1UP" />
-      <ArcadeText pos={[88, 0]} color="blue" text="HIGH SCORE" />
-      <ArcadeText pos={[216, 0]} color="red" text="2UP" />
+      <ArcadeText pos={[88, 0]} color="red" text="HIGH SCORE" />
+      <ArcadeText pos={[216, 0]} color="blue" text="2UP" />
       <Scoreboard pos={[0, 8]} score={game.players[0].score} />
       <Scoreboard pos={[104, 8]} score={game.highscore} />
       <Scoreboard pos={[200, 8]} score={0} />
