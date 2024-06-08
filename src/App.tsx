25d7d9d6b4ebb9799cdefc2955658f7f24e2fd17
Craig Smith <craig@littlejourney.health>
2024-06-08 09:42:44 +0100

    Added 'tap to start' screen, to force some user interaction.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import { Device, Engine } from "@overreact/engine";
 import { Arena, BottomBar, Game, LevelOverlay, Screen, TopBar } from "./components";
+import { ArcadeText } from "./components/ArcadeText";
 
 export const App = () => {
+  const [active, setActive] = useState(false);
+
   return (
     <Engine>
       <Device mode="desktop" bg="black" showFPS hideClose>
         <Screen size={[256, 240]} scale="auto">
-          <Game>
-            <TopBar />
-            <Arena />
-            <LevelOverlay />
-            <BottomBar />
-          </Game>
+          {active ? (
+            <Game>
+              <TopBar />
+              <Arena />
+              <LevelOverlay />
+              <BottomBar />
+            </Game>
+          ) : (
+            <div className="w-full h-full bg-black grid place-items-center" onClick={() => setActive(true)}>
+              <ArcadeText pos={[0, 0]} color="white" text="TAP TO BEGIN" />
+            </div>
+          )}
         </Screen>
       </Device>
     </Engine>
