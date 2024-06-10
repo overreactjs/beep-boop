2c5a34d92a5b321634a7a9a6da28a5e0de663c9b
Craig Smith <craig@littlejourney.health>
2024-06-10 20:25:38 +0100

    Added basic signals to all state entities (players, enemies, items, etc.)

--- src/state/EntityObjectState.ts
+++ src/state/EntityObjectState.ts
@@ -1,12 +1,37 @@
 import { Position, Property, VariableProperty, Velocity } from "@overreact/engine";
 import { PositionedObjectState } from "./PositionedObjectState";
 
+type Signal<T> = {
+  payload: T;
+}
+
 export class EntityObjectState extends PositionedObjectState {
 
   velocity: Property<Velocity>;
 
+  readonly signals: Map<string, Signal<unknown>> = new Map();
+
   constructor(pos: Position) {
     super(pos);
     this.velocity = new VariableProperty([0, 0]);
+  }
+
+  signal(type: string, payload?: unknown) {
+    this.signals.set(type, { payload });
+  }
+
+  handleSignal(type: string, fn: () => void) {
+    if (this.hasSignal(type)) {
+      this.clearSignal(type);
+      fn();
+    }
+  }
+
+  hasSignal(type: string) {
+    return this.signals.has(type);
+  }
+
+  clearSignal(type: string) {
+    this.signals.delete(type);
   }
 }
