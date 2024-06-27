6cd58670076a7a1a091222e92599b36707b45c3a
Craig Smith <craig@littlejourney.health>
2024-06-27 12:10:31 +0100

    Fixed a bug with flying bot not speeding up when 'hurry up' mode is activated.

--- src/components/Enemy/FlyingBot/states/usePatrolState.ts
+++ src/components/Enemy/FlyingBot/states/usePatrolState.ts
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import { StateFunction } from "@overreact/engine";
+import { StateFunction, useProperty } from "@overreact/engine";
 import { UseFlyingMovementResult } from "../../../../hooks";
 import { FlyingBotState } from "../../../../state";
 
@@ -7,6 +7,8 @@ const REGULAR_VELOCITY = [0.025, 0.015];
 const ANGRY_VELOCITY = [0.035, 0.021];
 
 export const usePatrolState = (movement: UseFlyingMovementResult): StateFunction<FlyingBotState> => {
+  const angry = useProperty(false);
+  
   return useCallback((fsm) => {
     if (fsm.age.current === 0) {
       const v = fsm.entity.angry.current ? ANGRY_VELOCITY : REGULAR_VELOCITY;
@@ -21,7 +23,14 @@ export const usePatrolState = (movement: UseFlyingMovementResult): StateFunction
     if (movement.flags.current.vertical) {
       fsm.entity.velocity.current[1] = -fsm.entity.velocity.current[1];
     }
-  }, [movement]);
+
+    // Update the speed of the bot if they have become angry, but were not previously.
+    if (!angry.current && fsm.entity.angry.current) {
+      fsm.entity.velocity.current[0] = Math.sign(fsm.entity.velocity.current[0]) * ANGRY_VELOCITY[0];
+      fsm.entity.velocity.current[1] = Math.sign(fsm.entity.velocity.current[1]) * ANGRY_VELOCITY[1];
+      angry.current = true;
+    }
+  }, [angry, movement]);
 };
 
 
