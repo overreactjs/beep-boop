1ac791ca398bbdab90aec8f27985b5fe71187bec
Craig Smith <craig@craigsmith.info>
2024-07-16 16:11:55 +0100

    Store and retrieve settings from local storage.

--- src/state/SettingsState.ts
+++ src/state/SettingsState.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BooleanProperty, CannedProperty } from "../types";
 
 export class SettingsState {
@@ -6,10 +7,27 @@ export class SettingsState {
   gameSpeed = new CannedProperty(1.0, [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]);
 
   static load() {
-    return new SettingsState();
+    const settings = new SettingsState();
+    const keys = Object.getOwnPropertyNames(settings);
+
+    keys.forEach((key) => {
+      const value = localStorage.getItem('setting_' + key);
+      console.log(key, value);
+      if (value !== null) {
+        (settings as any)[key].current = JSON.parse(value);
+      }
+    });
+
+    console.log(settings);
+
+    return settings;
   }
 
   save() {
-    // persist to local storage.
+    const keys = Object.getOwnPropertyNames(this);
+
+    keys.forEach((key) => {
+      localStorage.setItem('setting_' + key, JSON.stringify((this as any)[key].current))
+    });
   }
 }
