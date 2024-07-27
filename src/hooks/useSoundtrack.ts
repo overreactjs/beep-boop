a9f444e770dfb2f3d6a70f99b6e94b3e6aa42200
Craig Smith <craig@craigsmith.info>
2024-07-27 14:38:20 +0100

    Better comments.

--- src/hooks/useSoundtrack.ts
+++ src/hooks/useSoundtrack.ts
@@ -8,17 +8,31 @@ import HappyLevel from "../assets/music/HappyLevel.webm";
 import BossFight from "../assets/music/BossFight.webm";
 import SwingingLevel from "../assets/music/SwingingLevel.webm";
 
+/**
+ * Play the right soundtrack, based on the current game state and level. This hook uses `useMusic`
+ * in turn tags the audio to the 'music' so that playing a new track will cause the previuos track
+ * to stop.
+ */
 export const useSoundtrack = (game?: GameState) => {
   const music = useMusic();
 
   const track = useSync(() => {
-    return !game?.started.current
-      ? HappyLevel
-      : (game?.level.current || 0) > game.levels.length
-      ? SwingingLevel
-      : (game?.level.current || 0) % 20 === 0
-      ? BossFight
-      : MainTheme;
+    // Title screen
+    if (!game?.started.current) {
+      return HappyLevel;
+    }
+
+    // Ending screen
+    if ((game?.level.current || 0) > game.levels.length) {
+      return SwingingLevel;
+    }
+
+    // Boss fights (every 20 levels)
+    if ((game?.level.current || 0) % 20 === 0) {
+      return BossFight;
+    }
+
+    return MainTheme;
   });
 
   // Play either the main theme, or the boss fight music.
