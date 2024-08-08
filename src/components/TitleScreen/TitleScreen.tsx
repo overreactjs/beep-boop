024bd863d90c942c4eda70f8f1f360f47f31eb70
Craig Smith <craig@craigsmith.info>
2024-08-08 20:01:39 +0100

    Added title screen message to indicate the demo version.

--- src/components/TitleScreen/TitleScreen.tsx
+++ src/components/TitleScreen/TitleScreen.tsx
@@ -1,4 +1,4 @@
-import { BitmapImage, Box } from "@overreact/engine";
+import { BitmapImage, Box, useFlash } from "@overreact/engine";
 import { useAppState } from "../../hooks";
 import { Screen } from "../Screen";
 import { TitleMenu } from "./TitleMenu";
@@ -20,10 +20,22 @@ export const TitleScreen: React.FC<TitleScreenProps> = (props) => {
   const { onStart, onSettings, onCredits, onQuit } = props;
   const { state, go } = useAppState<TitleScreenState>('titleMenu');
 
+  const isDemo = import.meta.env.VITE_BUILD === 'demo';
+  const flash = useFlash(500);
+
+  console.log(import.meta.env.VITE_BUILD);
+
   return (
     <Screen size={[256, 240]} scale="auto">
       <Box pos={[0, 0]} size={[256, 240]} color="black">
         <BitmapImage image={LOGO} pos={[64, 8]} size={[128, 128]} offset={[0, 0]} />
+        {isDemo && (
+          <Box pos={[72, 96]} size={[112, 8]} color="#f00">
+            <ArcadeText pos={[0, 0]} text=" DEMO VERSION " color="white" />
+            <Box pos={[0, 0]} size={[112, 8]} color="#fff" className="mix-blend-difference" visible={flash} />
+            <Box pos={[0, 0]} size={[112, 8]} color="#f0f" className="mix-blend-multiply" />
+          </Box>
+        )}
         {state === 'titleMenu' && (
           <TitleMenu onNewGame={go('playersSelect')} onSettings={onSettings} onCredits={onCredits} onQuit={onQuit} />
         )}
